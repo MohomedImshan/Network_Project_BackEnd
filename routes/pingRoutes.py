68caaa74f7772e481ffa339b3f52ed412b0bc660
MohomedImshan <mohomedamccimshan@gmail.com>
2026-01-19 23:46:47 +0530

    added wifi details

--- routes/pingRoutes.py
+++ routes/pingRoutes.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from ping3 import ping
 
 ping_router = APIRouter()
 
 @ping_router.get("")
-def ping_host():
-    latency = ping("8.8.8.8", timeout=1)
-    if latency is None:
-        return {"ping": None}
-    return {"ping": int(latency * 1000)}
+
+async def ping_host():
+    try:
+        latency = ping("8.8.8.8", timeout=1)
+        return {"latency": latency}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+
