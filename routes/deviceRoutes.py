38c01a29dcc20f1f496b42e148e1e48a50d9d2ff
Ashan Sajana <ashansajana0623@gmail.com>
2026-01-27 19:23:31 +0530

    Enhance user and device management features; implement password reset and verification code functionality; update Wi-Fi info retrieval; optimize device scanning and traffic monitoring; refine project structure and dependencies

--- routes/deviceRoutes.py
+++ routes/deviceRoutes.py
@@ -1,14 +1,20 @@
 from fastapi import APIRouter, Depends
-from services.deviceServices import get_cached_devices
+from services.deviceServices import get_cached_devices, force_scan
 from dependencies.auth import get_current_user
 
-router = APIRouter(prefix="/devices", tags=["Devices"])
+router = APIRouter()
 
 
 @router.get("/getAllDevices")
 def get_devices(current_user=Depends(get_current_user)):
     devices = get_cached_devices()
     return {
-        "count": len(devices),
+        "ok": True,
         "devices": devices
     }
+
+
+@router.post("/refresh")
+def refresh_devices(current_user=Depends(get_current_user)):
+    force_scan()
+    return {"message": "Scan started"}
--- schemas/UserSchema.py
+++ schemas/UserSchema.py
@@ -14,3 +14,16 @@ class UserUpdate(BaseModel):
     firstName: str
     lastName: str
     email: EmailStr
+
+class PasswordResetRequest(BaseModel):
+    email: EmailStr
+    flow: str
+
+class PasswordResetVerify(BaseModel):
+    email: EmailStr
+    code: str
+    flow: str
+
+class ResetPassword(BaseModel):
+    email: EmailStr
+    new_password: str
